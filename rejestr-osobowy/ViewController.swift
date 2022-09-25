cc950df796948e880db7b1096c8464ce4ebed18c
Piotr Żakieta <piter5331@gmail.com>
2022-09-25 10:07:03 +0200

    Initial Commit

---
+++ rejestr-osobowy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rejestr-osobowy
+//
+//  Created by Piotr Żakieta on 25/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
